6a4f41301b2d6db5f90a72783629f36f214b8098
andermsilva <67654030+andermsilva@users.noreply.github.com>
2024-11-16 09:50:05 -0300

    teste de variaveis de ambiente

--- src/main/java/com/devsuperior/dslist/DslistApplication.java
+++ src/main/java/com/devsuperior/dslist/DslistApplication.java
@@ -8,6 +8,10 @@ public class DslistApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DslistApplication.class, args);
+
+		System.out.println("DB_URL: " + System.getenv("DB_URL"));
+		System.out.println("DB_USERNAME: " + System.getenv("DB_USERNAME"));
+		System.out.println("DB_PASSWORD: " + System.getenv("DB_PASSWORD"));
 	}
 
 }
