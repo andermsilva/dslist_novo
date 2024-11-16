0b0f37c6db61e1f38be4fbdae0fe97743c1bc55c
andermsilva <67654030+andermsilva@users.noreply.github.com>
2024-11-16 00:36:57 -0300

    refatorando delete files

--- src/main/java/com/devsuperior/dslist/repositories/GameListRepository.java
+++
@@ -1,8 +0,0 @@
-package com.devsuperior.dslist.repositories;
-
-import com.devsuperior.dslist.entities.GameList;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GameListRepository extends JpaRepository<GameList, Long> {
-
-}
